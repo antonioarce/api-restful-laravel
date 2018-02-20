fd1e0061242909a03cc5321b0ebbc9d23e3c297d
antonioarce <jlst.arce@gmail.com>
2018-02-19 22:27:43 -0400

    Implementacio de los modelos terminado

---
+++ app/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name', 'description', 'quantity', 'status', 'image', 'seller_id'
+    ];
+
+    public function estaDisponible(){
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->belongsToMany(Category::class);
+    }
+}
