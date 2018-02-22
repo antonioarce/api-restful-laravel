cc74fef12a466c65c94183e04faa289d80349753
antonioarce <jlst.arce@gmail.com>
2018-02-22 09:46:36 -0400

    Implemantacion de los controladores terminada, preparando la api para subida de imagenes

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\ApiController;
+use App\Product;
 use App\Seller;
+use function Composer\Autoload\includeFile;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -19,5 +22,65 @@ class SellerProductController extends ApiController
         $products = $seller->products;
 
         return $this->showAll($products);
+    }
+
+    public function store(Request $request, User $seller){
+        $rules = [
+            'name' => 'required' ,
+            'description' => 'required' ,
+            'quantity' => 'required|integer|min:1' ,
+            'image' => 'required|image' ,
+        ];
+        $this->validate($request, $rules);
+        $data = $request->all();
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+        $product = Product::create($data);
+        return $this->showAll($product);
+    }
+
+    public function update(Request $request, Seller $seller, Product $product){
+        $rules = [
+            'quantity' => 'required|min:1' ,
+            'status' => 'in:' . Product::PRODUCTO_NO_DISPONIBLE . ',' . Product::PRODUCTO_DISPONIBLE ,
+            'image' => 'image' ,
+        ];
+        $this->validate($request, $rules);
+        $this->verificarVendedor($seller,$product);
+
+        $product->fill($request->intersect([
+            'name', 'description', 'quantity'
+        ]));
+
+        if($request->has('status')){
+            $product->status = $request->status;
+            if($product->estaDisponible() && $product->categories()->count() ==0){
+                return $this->errorResponse('Un producto activo debe tener al menos una categoria', 409);
+            }
+        }
+
+        if($product->isClean()){
+            return $this->errorResponse('Se debe especificar almenos una valor diferente para actualizar',422);
+        }
+
+        $product->save();
+
+        return $this->showOne($product);
+    }
+
+    public function delete(Seller $seller, Product $product){
+
+        $this->verificarVendedor($seller,$product);
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function verificarVendedor(Seller $seller, Product $product){
+        if($seller->id != $product->seller_id){
+            throw new HttpException(422, 'El vendedor especificado no es el vendedor ereal del producto');
+        }
     }
 }
